7141f7cf879d0e1137e205929e782a8f2bc8b809
AndriyPyzh <andriy.pyzh2412@gmail.com>
2020-01-02 16:48:11 +0200

    added operator block handler

--- src/main/java/Command.java
+++ src/main/java/Command.java
@@ -4,22 +4,51 @@ import java.util.Arrays;
 import java.util.LinkedList;
 
 public class Command {
+
     private String command;
-    private String[] args = new String[3];
+    private String arg1;
+    private String arg2;
 
     public Command(String command, String arg1) {
         this.command = command;
-        this.args[0] = arg1;
+        this.arg1 = arg1;
     }
+
+
 
     public Command(String command, String arg1, String arg2) {
         this.command = command;
-        this.args[0] = arg1;
-        this.args[1] = arg2;
+        this.arg1 = arg1;
+        this.arg2 = arg2;
+    }
+
+    public String getCommand() {
+        return command;
+    }
+
+
+    public void setCommand(String command) {
+        this.command = command;
+    }
+
+    public String getArg1() {
+        return arg1;
+    }
+
+    public void setArg1(String arg1) {
+        this.arg1 = arg1;
+    }
+
+    public String getArg2() {
+        return arg2;
+    }
+
+    public void setArg2(String arg2) {
+        this.arg2 = arg2;
     }
 
     @Override
     public String toString() {
-        return command.toString() + ' ' + Arrays.toString(args);
+        return command.toString() + ' ' + arg1 + " " + arg2;
     }
 }
--- src/main/java/Compiler.java
+++ src/main/java/Compiler.java
@@ -1,25 +1,22 @@
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.LinkedList;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Compiler {
     private int index;
     private int temp_count;
-    private LinkedList<Token> tokens = new LinkedList<>();
+    private LinkedList<Token> tokens;
     private LinkedList<Command> COMMANDS = new LinkedList<>();
-
 
 
     public Compiler(LinkedList<Token> tokens) {
         index = 0;
         this.tokens = tokens;
-        temp_count=1;
+        temp_count = 1;
     }
 
-    private Token token(int indx){
+    private Token token(int indx) {
         return tokens.get(indx);
     }
 
@@ -40,22 +37,49 @@ public class Compiler {
     }
 
     private void HandleOperatorBlock() {
+        boolean isloop = token(index).isLoop();
+        index += 2;
+        int goto_index = COMMANDS.size();
+        String tempvar;
+        if (token(index + 1).getLexeme().equals("]")) {
+            tempvar = token(index).getLexeme();
+            index += 2;
+        } else {
+            tempvar = String.format("t%d", temp_count);
+            temp_count++;
+            HandleExpressionArg(tempvar);
+        }
+        index++;
+        COMMANDS.add(new Command("GOTOIFNOT", tempvar, "$"));
+        int gotoifnot_index = COMMANDS.size() - 1;
+        HandleBlock();
+        if (isloop) {
+            COMMANDS.add(new Command("GOTO", Integer.toString(goto_index)));
+        }
+        COMMANDS.get(gotoifnot_index).setArg2(Integer.toString(COMMANDS.size()));
+        index++;
+    }
 
+    private void HandleExpressionArg(String var) {
+        HandleExpression(var);
+    }
 
-
+    private void HandleExpressionArg() {
+        String var = "";
+        HandleExpression(var);
     }
 
     private void HandleCommand() {
         if (token(index).isIO()) {
             COMMANDS.add(new Command(token(index).getLexeme().toUpperCase(), token(index += 2).getLexeme()));
-        }else {
-            if(token(index+3).getLexeme().equals(";")){
-                COMMANDS.add(new Command(token(index).getLexeme(),token(index+=2).getLexeme()));
-            }else {
-                HandleExpression(token(index).getLexeme());
+        } else {
+            if (token(index + 3).getLexeme().equals(";")) {
+                COMMANDS.add(new Command(token(index).getLexeme(), token(index += 2).getLexeme()));
+            } else {
+                HandleExpressionArg(token(index).getLexeme());
             }
         }
-        index+=2;
+        index += 2;
     }
 
 
@@ -66,7 +90,7 @@ public class Compiler {
     public void WriteToFile(String path) throws IOException {
         FileWriter fileWriter = new FileWriter(path);
         PrintWriter printWriter = new PrintWriter(fileWriter);
-        COMMANDS.forEach((p)->printWriter.println(p.toString()));
+        COMMANDS.forEach((p) -> printWriter.println(p.toString()));
         printWriter.close();
     }
 
